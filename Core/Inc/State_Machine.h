3d4f4387d4df5dff106dc3390c9c00278352b492
Cybernetyc <Necromounger1990@gmail.com>
2025-12-03 22:39:55 +0300

    Added detailed comments to `State_Machine.c` and `State_Machine.h` for improved code documentation, including descriptions for functions, structures, and enums related to state management and valve control.

--- Core/Inc/State_Machine.h
+++ Core/Inc/State_Machine.h
@@ -10,21 +10,19 @@
 #include "stm32f4xx_hal.h"
 #include "main.h"
 
-/** Частные макроопределения */
-
+/** -- Частные макроопределения -- */
 /** -- Значения по умолчанию -- */
 #define DEFAULT_TIME  (3)               // Значение времени по умолчанию для cfg_sec в Machine_State_Context
 
 /** -- Макроопределения для клапана -- */
 #define VALVE_GPIO_PORT (VALVE_GPIO_Port)  // Порт клапана
 #define VALVE_PIN       (VALVE_Pin)        // Пин клапана
+/** --Окончание макроопределений --/
 
 /** Перечисления */
-
-/** -- Состояния машины -- */
-
 /**
  * @brief Перечисление состояний машины
+ *
  */
 typedef enum {
   STATE_READY     = 0,   // Готовность. Ожидание внешнего события
@@ -32,32 +30,43 @@ typedef enum {
   STATE_CONFIG    = 2    // Конфигурация.
 } MachineState_t;        // Состояние машины
 
-/** -- События машины -- */
+/**
+ * @brief Перечисления событий машины
+ *
+ */
 typedef enum {
-  EVENT_NONE = 0,             // Нет события (по-умолчанию)
-  EVENT_BTN_SHRT_PRESS = 1,   // Короткое нажатие кнопки
-  EVENT_BTN_LONG_PRESS = 2,   // Долгое нажатие кнопки
-  EVENT_TICK_1S        = 3    // 1-секундный тик таймера
-} MachineEvent_t;             // Событие для машины состояний
+  EVENT_NONE = 0,                 /// Нет события (по-умолчанию)
+  EVENT_BTN_SHRT_PRESS = 1,       /// Короткое нажатие кнопки
+  EVENT_BTN_LONG_PRESS = 2,       /// Долгое нажатие кнопки
+  EVENT_TICK_1S        = 3        /// 1-секундный тик таймера
+} MachineEvent_t;                 /// Событие для машины состояний
 
-/** -- Положение клапана -- */
+/**
+ * @brief Перечисления состояний клапана
+ *
+ */
 typedef enum {
-  CLOSED = 0,                 // Клапан закрыт
-  OPEN   = 1                  // Клапана открыт
-} Valve_State_t;              // Состояние клапана
+  CLOSED = 0,                    /// Клапан закрыт
+  OPEN   = 1                     /// Клапана открыт
+} Valve_State_t;                 /// Состояние клапана
+/** Окончание перечислений */
 
 /** Структуры */
-
-/** -- Структура контекста состояния машины -- */
+/**
+ * @brief Структура контекста состояния машины
+ */
 typedef struct {
   MachineState_t machine_state;  /// Current condition of the machine
   Valve_State_t  valve_state  ;  /// Valve flag state
   uint8_t cfg_sec ;              /// Start value of seconds
   uint8_t cur_sec ;              /// Current value of seconds
-} MachineState_Context_t;        /// Context of Machine State
+}MachineState_Context_t;        /// Context of Machine State
 
 
 /** -- Прототипы функций -- */
+/**
+ * @brief
+ */
 void Machine_Process (MachineState_Context_t* ctx, MachineEvent_t event);
 
 #endif //INC_7_SEG_STATE_MACHINE_H
--- Core/Src/State_Machine.c
+++ Core/Src/State_Machine.c
@@ -7,14 +7,50 @@
 
 #include "AppFlashConfig.h"
 
+/**
+  * @brief Дескриптор структуры для управления 7-сегментным индикатором.
+  *
+  * @details Эта структура используется для хранения и управления
+  * конфигурацией и состоянием 7-сегментного дисплея,
+  * включая цифровые порты, выводы, сегментный порт,
+  * активную маску контактов и текущее состояние цифр.
+  * Он позволяет взаимодействовать с дисплеем через соответствующие функции драйвера.
+  */
 extern Seg7_Handle_t seg7_handle;
 
+/**
+  * @brief Устанавливает состояние клапана и обновляет контекст машины.
+  *
+  * @details Эта функция устанавливает контакт GPIO,
+  * связанный с клапаном, в желаемое состояние и
+  * обновляет состояние клапана в указанном контексте состояния машины,
+  * чтобы отразить изменение.
+  *
+  * @param ctx Указатель на структуру контекста состояния машины,
+  *            содержащую текущее состояние машины.
+  * @param Valve_state_set Желаемое состояние для установки клапана
+  *                        (ОТКРЫТО или ЗАКРЫТО).
+  */
 static inline void Valve_Set (MachineState_Context_t* ctx, const Valve_State_t Valve_state_set)
 {
   HAL_GPIO_WritePin(VALVE_GPIO_PORT, VALVE_PIN, (Valve_state_set ? GPIO_PIN_RESET : GPIO_PIN_SET));
   ctx->valve_state = Valve_state_set;
 }
 
+/**
+  * @brief Retrieves the next value based on a predefined sequence of input.
+  *
+  * This function determines the next value in a circular sequence
+  * depending on the provided input value, with a default fall-through value
+  * if the input does not match any of the specific cases.
+  *
+  * @param input_value The current value to evaluate and determine its next sequence value.
+  * @return The next value in the sequence. If the input value matches:
+  *         - DEFAULT_TIME, returns DEFAULT_TIME+1.
+  *         - DEFAULT_TIME+1, returns DEFAULT_TIME+2.
+  *         - 5, returns 6.
+  *         - Any other value, returns 3 as the default.
+  */
 static inline uint8_t cfg_next_3_6 (const uint8_t input_value)
 {
   switch (input_value)
@@ -30,11 +66,20 @@ static inline uint8_t cfg_next_3_6 (const uint8_t input_value)
   }
 }
 
+
 /**
- * @brief Base machine process template
- * @param ctx
- * @param event
- */
+  * @brief Processes the state machine transitions and actions based on the current state and events.
+  *
+  * This function manages state transitions for a state machine, which can operate in three primary
+  * modes: READY, COUNTDOWN, and CONFIG. Actions are performed depending on the current state and
+  * received event, including updating the machine's state, opening or closing a valve, modifying
+  * configuration values, and updating a seven-segment display.
+  *
+  * @param ctx Pointer to the machine state context structure, which contains information
+  *        about the machine's current state and configuration.
+  * @param event The current event triggering the state machine to process, such as a button press
+  *        or a timer tick.
+  */
 void Machine_Process (MachineState_Context_t* ctx, const MachineEvent_t event)
 {
   switch (ctx->machine_state)
@@ -62,7 +107,7 @@ void Machine_Process (MachineState_Context_t* ctx, const MachineEvent_t event)
       {
         ctx->machine_state  = STATE_READY;
         Valve_Set(ctx, CLOSED);
-        //ctx->valve_state    = CLOSED;
+        //ctx->valve_state   = CLOSED;
       }
 
       if (event == EVENT_TICK_1S)            /// Событие - 1 секунда
